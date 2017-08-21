e0d4ab77f678c2783e0b3c26d06802d5f22351c3
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-21 23:31:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var recipes = {
 };
 
 function updateObjectWithKeyAndValue(object, key, value){
-object[key]=value;
-return object;
+var newObject = object;
+newObject[key]=value;
+return newObject;
 }
 
 // function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
