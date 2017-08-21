985b471af8588900ad38f59c99a7db59e3e7e247
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-21 23:58:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ function deleteFromObjectByKey(object, key){
   return object;
 }
 
+destructivelyDeleteFromObjectByKey(object,key){
+  delete object.key;
+  return object;
+}
  updateObjectWithKeyAndValue(recipes,"ingredient4","cinnamon");
  destructivelyUpdateObjectWithKeyAndValue(recipes,"ingredient5","lemon");
  deleteFromObjectByKey(recipes,"ingredient1");
